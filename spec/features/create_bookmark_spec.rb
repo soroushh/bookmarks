057e650f321e65f8e39b483ea4ee2b70551b37c9
Amina Jusic <amina_jusic@yahoo.com>
2019-02-28 16:14:18 +0000

    Step 11, Wrapping Database Data in Program Objects is Done

--- spec/features/create_bookmark_spec.rb
+++ spec/features/create_bookmark_spec.rb
@@ -2,7 +2,8 @@ feature "Adding new bookmark" do
   scenario " I want to be able to make a bookmark" do
     visit "/bookmarks/new"
     fill_in('url',with: "http://testbookmark.com")
+    fill_in('title', with: "Test bookmark")
     click_button "Submit"
-    expect(page).to have_content "http://testbookmark.com"
+    expect(page).to have_link("Test bookmark", href: "http://testbookmark.com")
   end
 end
